9aa2c2cf990243049b3618a9fd4391946779eb6b
Zak G <zakgriffith@gmail.com>
2024-08-02 11:01:50 -0600

    Update Welcome.tsx

--- packages/nextjs/components/homepage/Welcome.tsx
+++ packages/nextjs/components/homepage/Welcome.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Welcome = () => {
   return (
     <div className="md:text-base text-[0.8rem] border rounded-xl p-4 text-center">
-      <h1 className="font-bold font-typo-round md:text-2xl text-xl ">BuidlGuild Workshop Streams</h1>
+      <h1 className="font-bold font-typo-round md:text-2xl text-xl ">BuidlGuild Node Streams</h1>
       <p>
         We&apos;re funding BuidlGuidl members for building and maintaining our Nodes program. We are creating scripts to
         quickly create and monitor nodes, building and shipping synced nodes to underserved areas, and providing RPC
