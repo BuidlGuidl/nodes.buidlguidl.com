9e7f9b2986f1cd731fe2e53ffeb9620eb36a3cf2
Zak G <zakgriffith@gmail.com>
2024-08-06 12:50:32 -0600

    Add link to BG client

--- packages/nextjs/components/homepage/Welcome.tsx
+++ packages/nextjs/components/homepage/Welcome.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-
 const Welcome = () => {
   return (
     <div className="md:text-base text-[0.8rem] border rounded-xl p-4 text-center">
       <h1 className="font-bold font-typo-round md:text-2xl text-xl ">BuidlGuild Node Streams</h1>
       <p>
-        We&apos;re funding BuidlGuidl members for building and maintaining our Nodes program. We are creating scripts to
-        quickly create and monitor nodes, building and shipping synced nodes to underserved areas, and providing RPC
-        access.
+        We&apos;re funding BuidlGuidl members for creating and maintaining the
+        <p>
+          <a href="https://client.buidlguidl.com">
+            <button className="btn btn-primary btn-lg" type="button">
+              BuidlGuidl Node Client
+            </button>
+          </a>
+        </p>
+        a one line command to deploy and monitor an Ethereum Node. We are also building and shipping fully synced nodes
+        to underserved areas around the world.
       </p>
     </div>
   );
