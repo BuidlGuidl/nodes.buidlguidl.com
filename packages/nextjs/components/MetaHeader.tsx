342cb4b9b5aa7203c679ba9a28fbd75a2d7038d1
Zak G <zakgriffith@gmail.com>
2024-08-08 17:06:01 -0600

    Update MetaHeader.tsx

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -9,7 +9,7 @@ type MetaHeaderProps = {
   children?: React.ReactNode;
 };
 
-const baseUrl = "https://buidlguidlpod.vercel.app/"; //Need to manually put in the base url
+const baseUrl = "https://nodes.buidlguidl.com/"; //Need to manually put in the base url
 
 export const MetaHeader = ({
   title = "BuidlGuidl Nodes",
